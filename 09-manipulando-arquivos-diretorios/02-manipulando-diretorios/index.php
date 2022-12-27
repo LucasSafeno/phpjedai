0d7f30c740d7007daeaf9a5b94fbb84ed9f9878d
Lucas Safeno <lugnorio@gmail.com>
2022-12-27 11:31:02 -0300

    PHP Jedai - Manipulando Arquivos e Diretórios

---
+++ 09-manipulando-arquivos-diretorios/02-manipulando-diretorios/index.php
@@ -0,0 +1,18 @@
+<?php 
+	// cria pasta
+	//mkdr('pasta')
+	
+	/*
+	if(is_dir('pasta')){
+		echo 'É uma pasta válida';
+	}else{
+		echo 'Não existe ou não é um diretório <br>';
+		mkdir('pasta');
+		echo 'Pasta criada!';
+	} */
+
+	// Deletar pasta
+	//rmdir('pasta')
+
+
+ ?>
